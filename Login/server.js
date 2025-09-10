86ae10855db9cdde5f58a269be800560bf2c1cd6
Milton M <mwmerenxar@gmail.com>
2025-09-09 19:18:49 -0600

    Mongo y server funcionando

--- Login/server.js
+++ Login/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const User = require("./models/ User");
 const bodyParser = require("body-parser");
 const path = require("path");
 
@@ -13,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Archivos estáticos (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname, "public")));
 
-// "Base de datos" temporal
+// Base de datos temporal
 const USERS = {
   usuario: "1234", // usuario: contraseña
   admin: "adminpass"
@@ -26,16 +27,26 @@ mongoose.connect("mongodb://127.0.0.1:27017/miLogin",{
 .then(()=> console.log("conectado a MongoDB"))
 .catch(err=> console.error("Error al conectar: ",err))
 
-// Ruta para procesar login
-app.post("/login", (req, res) => {
-  const { username, password } = req.body;
-
-  if (USERS[username] && USERS[username] === password) {
-    res.json({ success: true, message: "Inicio de sesión exitoso ✅" });
-  } else {
-    res.json({ success: false, message: "Usuario o contraseña incorrectos ❌" });
+//Rutas
+app.post("/register",async(req,res)=>{
+  try{
+    const nuevoUsuario=new User(req.body);
+    await nuevoUsuario.save();
+    res.status(201).json({mensaje:"USUARIO CREADO", usuario : nuevoUsuario});
+  }catch (err){
+    res.status(400).json({error: err.message});
   }
-});
+})
+
+app.post("/login",async(req,res)=>{
+  const{email,password}= req.body;
+  const usuario= await User.findOne({email,password});
+  if(!usuario)
+    return res.status(401).json({mensaje:"Usuario Incorrecto"});
+  res.json({mensaje:"Login Exitoso",rol:usuario.rol, usuario});
+})
+
+// Iniciando
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
